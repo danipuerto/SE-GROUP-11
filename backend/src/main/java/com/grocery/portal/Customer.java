0ec1d73d2ed9aec748b78e827fc18e5ed06dda3e
danipuerto <daniel.puerto@my.utsa.edu>
2026-07-28 21:23:44 -0500

    Update Customer.java

--- backend/src/main/java/com/grocery/portal/Customer.java
+++ backend/src/main/java/com/grocery/portal/Customer.java
@@ -1,5 +1,7 @@
 package com.grocery.portal;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -12,6 +14,7 @@ public class Customer {
     private String firstName;
     private String lastName;
     private String email;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     private String phone;
 
